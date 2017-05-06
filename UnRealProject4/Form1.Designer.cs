89f60c718feb3b0357ca42588307a5b57ac76711
B1010 <artemik-98@outlook.com>
2017-05-06 09:13:12 +0400

    Кода нет, но вы держитесь!

--- UnRealProject4/Form1.Designer.cs
+++ UnRealProject4/Form1.Designer.cs
@@ -95,6 +95,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Поиск";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // tableLayoutPanel1
             // 
--- UnRealProject4/Form1.cs
+++ UnRealProject4/Form1.cs
@@ -16,5 +16,10 @@ namespace UnRealProject4
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
